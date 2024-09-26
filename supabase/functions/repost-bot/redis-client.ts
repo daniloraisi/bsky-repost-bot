ef2a42a74fb6061b749150332881e6b8d4d6931d
Danilo Raisi <danilo@raisi.tech>
2024-09-26 17:58:28 -0300

    fix: Change redis validation

--- supabase/functions/repost-bot/redis-client.ts
+++ supabase/functions/repost-bot/redis-client.ts
@@ -1,14 +1,13 @@
 import { Redis } from "npm:@upstash/redis";
 
-const redis = new Redis({
-  url: Deno.env.get("UPSTASH_REDIS_REST_URL")!,
-  token: Deno.env.get("UPSTASH_REDIS_REST_TOKEN")!,
+const redis = Redis.fromEnv({
+  enableAutoPipelining: false,
 });
 
 export async function mentionReposted(cid: string) {
-  const exists = await redis.exists(cid);
+  const exists = await redis.get(cid);
 
-  return exists === 1;
+  return exists && exists === "r";
 }
 
 export async function saveMention(cid: string) {
